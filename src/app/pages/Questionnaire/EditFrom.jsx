2978446b6e6137e517e9eae589aae86e40ff57f3
Taalaibekovi4 <148772481+Taalaibekovi4@users.noreply.github.com>
2026-04-05 20:57:29 +0600

    2
    2

--- src/app/pages/Questionnaire/EditFrom.jsx
+++ src/app/pages/Questionnaire/EditFrom.jsx
@@ -1,256 +1,256 @@
-// import {
-//   getQuestionnaire,
-//   putDataQuestionnaire,
-// } from "@/app/store/slice/questionnaire-get";
-// import React, { useEffect } from "react";
-// import { useForm } from "react-hook-form";
-// import { useDispatch, useSelector } from "react-redux";
-// import s from "./page2.module.scss";
-// import { InputMask } from "@react-input/mask";
-// import { ToastContainer, toast } from "react-toastify";
-// import cm from "classnames";
-// import { Loader } from "@/components/Loader/Loader";
+import {
+  getQuestionnaire,
+  putDataQuestionnaire,
+} from "@/app/store/slice/questionnaire-get";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import s from "./page2.module.scss";
+import { InputMask } from "@react-input/mask";
+import { ToastContainer, toast } from "react-toastify";
+import cm from "classnames";
+import { Loader } from "@/components/Loader/Loader";
 
-// export function EditFrom() {
-//   const {
-//     register,
-//     handleSubmit,
-//     formState: { errors, isDirty },
-//   } = useForm();
+export function EditFrom() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty },
+  } = useForm();
 
-//   const { status, data } = useSelector((state) => state.questionnaireGetSlice);
+  const { status, data } = useSelector((state) => state.questionnaireGetSlice);
 
-//   const dispatch = useDispatch();
-//   const onSubmit = (data) => {
-//     dispatch(putDataQuestionnaire(data));
-//     toast.success("Успешно отправлено ", {
-//       position: "bottom-center",
-//       autoClose: 5000,
-//       hideProgressBar: false,
-//       closeOnClick: true,
-//       pauseOnHover: true,
-//       draggable: true,
-//       progress: undefined,
-//       theme: "light",
-//     });
-//   };
+  const dispatch = useDispatch();
+  const onSubmit = (data) => {
+    dispatch(putDataQuestionnaire(data));
+    toast.success("Успешно отправлено ", {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  };
 
-//   const fromData = [
-//     {
-//       name: "full_name",
-//       title: "Ф.И.О.",
-//       maxLength: 255,
-//       xNullable: true,
-//       value: data?.full_name,
-//     },
-//     {
-//       name: "actual_residence",
-//       title: "Место фактического проживания",
-//       maxLength: 255,
-//       xNullable: true,
-//       value: data?.actual_residence,
-//     },
-//     {
-//       name: "phone",
-//       title: "Номер телефона",
-//       maxLength: 255,
-//       xNullable: true,
-//       value: data?.phone,
-//     },
-//     {
-//       name: "work_address",
-//       title: "Место работы",
-//       maxLength: 255,
-//       xNullable: true,
-//       value: data?.work_address,
-//     },
-//     {
-//       name: "position",
-//       title: "Должность",
-//       maxLength: 255,
-//       xNullable: true,
-//       value: data?.position,
-//     },
-//     {
-//       name: "avg_salary",
-//       title: "Средний заработок в месяц (в сомах)",
-//       type: "number",
-//       placeholder: "Средний заработок",
-//       maxLength: 255,
-//       xNullable: true,
-//       value: data?.avg_salary,
-//     },
-//     {
-//       name: "whatsapp",
+  const fromData = [
+    {
+      name: "full_name",
+      title: "Ф.И.О.",
+      maxLength: 255,
+      xNullable: true,
+      value: data?.full_name,
+    },
+    {
+      name: "actual_residence",
+      title: "Место фактического проживания",
+      maxLength: 255,
+      xNullable: true,
+      value: data?.actual_residence,
+    },
+    {
+      name: "phone",
+      title: "Номер телефона",
+      maxLength: 255,
+      xNullable: true,
+      value: data?.phone,
+    },
+    {
+      name: "work_address",
+      title: "Место работы",
+      maxLength: 255,
+      xNullable: true,
+      value: data?.work_address,
+    },
+    {
+      name: "position",
+      title: "Должность",
+      maxLength: 255,
+      xNullable: true,
+      value: data?.position,
+    },
+    {
+      name: "avg_salary",
+      title: "Средний заработок в месяц (в сомах)",
+      type: "number",
+      placeholder: "Средний заработок",
+      maxLength: 255,
+      xNullable: true,
+      value: data?.avg_salary,
+    },
+    {
+      name: "whatsapp",
 
-//       title: "Whatsapp",
-//       maxLength: 255,
-//       xNullable: true,
-//       value: data?.whatsapp,
-//     },
-//     {
-//       name: "instagram",
-//       title: "Instagram",
-//       maxLength: 255,
-//       xNullable: true,
-//       value: data?.instagram,
-//     },
-//   ];
-//   const fromData2 = [
-//     {
-//       name: "guarantor_full_name_1",
-//       title: "Ф.И.О.",
-//       maxLength: 255,
-//       xNullable: true,
-//       value: data?.guarantor_full_name_1,
-//     },
-//     {
-//       name: "guarantor_related_1",
-//       title: "Кем приходится ",
-//       maxLength: 255,
-//       xNullable: true,
-//       value: data?.guarantor_related_1,
-//     },
-//     {
-//       name: "guarantor_address_1",
-//       title: "Адрес проживания ",
-//       maxLength: 255,
-//       xNullable: true,
-//       value: data?.guarantor_address_1,
-//     },
-//     {
-//       name: "guarantor_phone_1",
-//       title: "Номер телефона ",
-//       maxLength: 255,
-//       xNullable: true,
-//       value: data?.guarantor_phone_1,
-//     },
-//     {
-//       name: "guarantor_full_name_2",
-//       title: "Ф.И.О.",
-//       maxLength: 255,
-//       xNullable: true,
-//       value: data?.guarantor_full_name_2,
-//     },
-//     {
-//       name: "guarantor_related_2",
-//       title: "Кем приходится ",
-//       maxLength: 255,
-//       xNullable: true,
-//       value: data?.guarantor_related_2,
-//     },
-//     {
-//       name: "guarantor_address_2",
-//       title: "Адрес проживания ",
-//       maxLength: 255,
-//       xNullable: true,
-//       value: data?.guarantor_address_2,
-//     },
-//     {
-//       name: "guarantor_phone_2",
-//       title: "Номер телефона №2",
-//       maxLength: 255,
-//       xNullable: true,
-//       value: data?.guarantor_phone_2,
-//     },
-//   ];
-//   return (
-//     <div className={`${s.Blocks} container`}>
-//       <ToastContainer
-//         className={s.ToastContainer}
-//         autoClose={1000}
-//         limit={1}
-//         hideProgressBar
-//         newestOnTop={false}
-//         closeOnClick
-//         rtl={false}
-//         pauseOnFocusLoss
-//         draggable
-//         pauseOnHover
-//         theme="dark"
-//       />
-//       {status === "loading" && <Loader />}
-//       {status === "succeeded" && (
-//         <form
-//           className={s.block}
-//           onSubmit={handleSubmit(onSubmit)}
-//           encType="multipart/form-data"
-//         >
-//           <h2>Анкета заявителя</h2>
+      title: "Whatsapp",
+      maxLength: 255,
+      xNullable: true,
+      value: data?.whatsapp,
+    },
+    {
+      name: "instagram",
+      title: "Instagram",
+      maxLength: 255,
+      xNullable: true,
+      value: data?.instagram,
+    },
+  ];
+  const fromData2 = [
+    {
+      name: "guarantor_full_name_1",
+      title: "Ф.И.О.",
+      maxLength: 255,
+      xNullable: true,
+      value: data?.guarantor_full_name_1,
+    },
+    {
+      name: "guarantor_related_1",
+      title: "Кем приходится ",
+      maxLength: 255,
+      xNullable: true,
+      value: data?.guarantor_related_1,
+    },
+    {
+      name: "guarantor_address_1",
+      title: "Адрес проживания ",
+      maxLength: 255,
+      xNullable: true,
+      value: data?.guarantor_address_1,
+    },
+    {
+      name: "guarantor_phone_1",
+      title: "Номер телефона ",
+      maxLength: 255,
+      xNullable: true,
+      value: data?.guarantor_phone_1,
+    },
+    {
+      name: "guarantor_full_name_2",
+      title: "Ф.И.О.",
+      maxLength: 255,
+      xNullable: true,
+      value: data?.guarantor_full_name_2,
+    },
+    {
+      name: "guarantor_related_2",
+      title: "Кем приходится ",
+      maxLength: 255,
+      xNullable: true,
+      value: data?.guarantor_related_2,
+    },
+    {
+      name: "guarantor_address_2",
+      title: "Адрес проживания ",
+      maxLength: 255,
+      xNullable: true,
+      value: data?.guarantor_address_2,
+    },
+    {
+      name: "guarantor_phone_2",
+      title: "Номер телефона №2",
+      maxLength: 255,
+      xNullable: true,
+      value: data?.guarantor_phone_2,
+    },
+  ];
+  return (
+    <div className={`${s.Blocks} container`}>
+      <ToastContainer
+        className={s.ToastContainer}
+        autoClose={1000}
+        limit={1}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      {status === "loading" && <Loader />}
+      {status === "succeeded" && (
+        <form
+          className={s.block}
+          onSubmit={handleSubmit(onSubmit)}
+          encType="multipart/form-data"
+        >
+          <h2>Анкета заявителя</h2>
 
-//           {fromData.map((item, i) => (
-//             <div className={s.anketa} key={i}>
-//               <label htmlFor="">{item.title}</label>
+          {fromData.map((item, i) => (
+            <div className={s.anketa} key={i}>
+              <label htmlFor="">{item.title}</label>
 
-//               {item.name === "guarantor_phone_1" ||
-//               item.name === "guarantor_phone_2" ||
-//               item.name === "phone" ? (
-//                 // <InputMask
-//                 //   {...register(item.name ,{
-//                 //     required: 'Поле обязательно к заполнению',
-//                 //     maxLength: {
-//                 //       value: item.maxLength,
-//                 //       message: `Максимальная длина ${item.maxLength} символов`,
-//                 //     }
-//                 //   })}
-//                 //   mask="+7 (999) 999-99-99"
-//                 //   placeholder={item.title}
-//                 // />
-//                 <InputMask
-//                   placeholder="+996(ХХХ) ХХ-ХХ-ХХ"
-//                   mask="+996 (___) __-__-__"
-//                   {...register(item.name)}
-//                   value={item.value}
-//                   replacement={{ _: /\d/ }}
-//                 />
-//               ) : (
-//                 <input
-//                   {...register(item.name)}
-//                   placeholder={item.placeholder || item.title}
-//                   type={item.type || "text"}
-//                   defaultValue={item.value}
-//                 />
-//               )}
-//               {errors[item.name] && (
-//                 <p className={s.error}>{errors[item.name].message}</p>
-//               )}
-//             </div>
-//           ))}
+              {item.name === "guarantor_phone_1" ||
+              item.name === "guarantor_phone_2" ||
+              item.name === "phone" ? (
+                // <InputMask
+                //   {...register(item.name ,{
+                //     required: 'Поле обязательно к заполнению',
+                //     maxLength: {
+                //       value: item.maxLength,
+                //       message: `Максимальная длина ${item.maxLength} символов`,
+                //     }
+                //   })}
+                //   mask="+7 (999) 999-99-99"
+                //   placeholder={item.title}
+                // />
+                <InputMask
+                  placeholder="+996(ХХХ) ХХ-ХХ-ХХ"
+                  mask="+996 (___) __-__-__"
+                  {...register(item.name)}
+                  value={item.value}
+                  replacement={{ _: /\d/ }}
+                />
+              ) : (
+                <input
+                  {...register(item.name)}
+                  placeholder={item.placeholder || item.title}
+                  type={item.type || "text"}
+                  defaultValue={item.value}
+                />
+              )}
+              {errors[item.name] && (
+                <p className={s.error}>{errors[item.name].message}</p>
+              )}
+            </div>
+          ))}
 
-//           <h2>Контактные данные родных</h2>
+          <h2>Контактные данные родных</h2>
 
-//           {fromData2.map((item, i) => (
-//             <div className={s.anketa} key={i}>
-//               <label htmlFor="">{item.title}</label>
-//               {item.name === "guarantor_phone_1" ||
-//               item.name === "guarantor_phone_2" ? (
-//                 <InputMask
-//                   placeholder="+996(ХХХ) ХХ-ХХ-ХХ"
-//                   mask="+996 (___) __-__-__"
-//                   {...register(item.name)}
-//                   replacement={{ _: /\d/ }}
-//                   value={item.value}
-//                 />
-//               ) : (
-//                 <input
-//                   {...register(item.name)}
-//                   placeholder={item.title}
-//                   defaultValue={item.value}
-//                 />
-//               )}
-//               {errors[item.name] && (
-//                 <p className={s.error}>{errors[item.name].message}</p>
-//               )}
-//             </div>
-//           ))}
-//           <button
-//             disabled={!isDirty}
-//             className={cm(s.button, isDirty && s.btn_orange)}
-//           >
-//             Сохранить изменения
-//           </button>
-//         </form>
-//       )}
-//     </div>
-//   );
-// }
+          {fromData2.map((item, i) => (
+            <div className={s.anketa} key={i}>
+              <label htmlFor="">{item.title}</label>
+              {item.name === "guarantor_phone_1" ||
+              item.name === "guarantor_phone_2" ? (
+                <InputMask
+                  placeholder="+996(ХХХ) ХХ-ХХ-ХХ"
+                  mask="+996 (___) __-__-__"
+                  {...register(item.name)}
+                  replacement={{ _: /\d/ }}
+                  value={item.value}
+                />
+              ) : (
+                <input
+                  {...register(item.name)}
+                  placeholder={item.title}
+                  defaultValue={item.value}
+                />
+              )}
+              {errors[item.name] && (
+                <p className={s.error}>{errors[item.name].message}</p>
+              )}
+            </div>
+          ))}
+          <button
+            disabled={!isDirty}
+            className={cm(s.button, isDirty && s.btn_orange)}
+          >
+            Сохранить изменения
+          </button>
+        </form>
+      )}
+    </div>
+  );
+}
--- src/app/pages/Questionnaire/layout.jsx
+++ src/app/pages/Questionnaire/layout.jsx
@@ -1,14 +1,14 @@
-// export const metadata = {
-//   title: "Электроника сервис",
-//   description: " ",
-//   icons: [
-//     {
-//       rel: "icon",
-//       sizes: "any",
-//       url: "/img/iconka.png",
-//     },
-//   ],
-// };
-// export default function storyLayout({ children }) {
-//   return <main>{children}</main>;
-// }
+export const metadata = {
+  title: "Электроника сервис",
+  description: " ",
+  icons: [
+    {
+      rel: "icon",
+      sizes: "any",
+      url: "/img/iconka.png",
+    },
+  ],
+};
+export default function storyLayout({ children }) {
+  return <main>{children}</main>;
+}
